e27c85400fba1a157f46c62a512c5177ca5c4985
zwsut <zwsutherland@gmail.com>
2024-11-21 19:55:42 -0500

    images alt text update

--- utilities/index.js
+++ utilities/index.js
@@ -36,8 +36,8 @@ Util.buildClassificationGrid = async function(data){
       grid += '<li>'
       grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
       + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
-      + 'details"><img src="' + vehicle.inv_thumbnail 
-      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+      + ' details"><img src="' + vehicle.inv_thumbnail 
+      +'" alt="'+ vehicle.inv_make + ' ' + vehicle.inv_model 
       +' on CSE Motors" /></a>'
       grid += '<div class="namePrice">'
       grid += '<hr />'
@@ -72,7 +72,7 @@ Util.buildVehiclePage = async function (data) {
 
     // Image - Left on desktop
     vehicleTemplate += '<div class="vehicle-image-container">';
-    vehicleTemplate += `<img src="${data.inv_image}" alt="Image of ${data.inv_make} ${data.inv_model}">`;
+    vehicleTemplate += `<img src="${data.inv_image}" alt="${data.inv_make} ${data.inv_model}">`;
     vehicleTemplate += "</div>";
 
     // Details - right on desktop
